e495addcbf56e5121c012373c2449b7e5d1fa456
Szymon Cader <szymon.sc.cader@gmail.com>
2022-11-27 00:42:04 +0100

    Add custom logo

--- iot/settings/jazzmin.py
+++ iot/settings/jazzmin.py
@@ -3,7 +3,7 @@ JAZZMIN_SETTINGS = {
     "site_header": "Iot",
     "site_brand": "IoT Administration",
     "site_header_title": "IoT",
-    "site_header_logo": "https://www.hivemq.com/wp-content/uploads/2019/01/hivemq-logo.png",
+    "site_logo": "img/logo.jpg",
     "welcome_sign": "Welcome to IoT",
     "search_model": "auth.user",
     "show_ui_builder": True,
