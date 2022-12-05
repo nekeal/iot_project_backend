2baf7ad0b6c81dbde6fcc84a3f59b052486bed66
karolwn <karolwn35@gmail.com>
2022-12-05 20:58:01 +0100

    fix for displaying the fetched results

--- iot/boxes/views.py
+++ iot/boxes/views.py
@@ -42,6 +42,7 @@ class GetMessageFormView(FormView):
     form_class = GetMessageForm
 
     template_name = "listen_message.html"
+    query_result = []
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -49,19 +50,17 @@ class GetMessageFormView(FormView):
             **context,
             **admin_site.each_context(self.request),
             "media": self.form_class().media,
+            "response": [ "date: {}, message: {}".format(result.fetch_date, result.message) for result in self.query_result ],
         }
 
     def form_valid(self, form):
-        messages.success(self.request, "Message fetched")
+        messages.success(self.request, "Data loaded sucesfully")
         return self.get(self.request)
 
     def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         form = self.get_form()
         if form.is_valid():
-            query_result = TopicMessages.objects.filter(topic=form.cleaned_data["topic"]).order_by("-fetch_date")
-            parsed_query = [ "date: {}, message: {}".format(result.fetch_date, result.message) for result in query_result ]
-            for msg in parsed_query:
-                messages.success(self.request, msg)
+            self.query_result = TopicMessages.objects.filter(topic=form.cleaned_data["topic"]).order_by("-fetch_date")
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
